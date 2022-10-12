362ac7811f589e8913d9cf36d35ebc2e1663d39a
kiy17 <fricky1804@gmail.com>
2022-10-12 21:51:20 +0700

    diskusi2

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h2>Mahasiswa Universitas Terbuka : </h2>
+    <h3>Nama &ensp;: {{ nama }} </h3>
+    <h3>Nim &emsp; : {{ nim }} </h3>
+    <h3>UPBJJ : {{ upbjj }} </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      nama: "Aggitt Dema Alfaridzi",
+      nim: "041812168",
+      upbjj: "Malang",
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
